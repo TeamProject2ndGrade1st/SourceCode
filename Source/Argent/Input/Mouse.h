5de47e596bd05e794284057736a1bd9fb04a573b
ShinG24 <ShinGgm24@gmail.com>
2023-04-20 16:09:32 +0900

    マウスを中心に固定できるようにした　F!1で終了できる リリース見てる

--- Source/Argent/Input/Mouse.h
+++ Source/Argent/Input/Mouse.h
@@ -52,6 +52,8 @@ namespace Argent::Input
 		float GetRowWheelRotateValue() const { return rowWheelRotateValue; }
 
 		std::unordered_map<Button, State> mouseState;
+
+		void SetIsResetPositionToCenter(bool b) { resetPositionToCenter = b; }
 	private:
 		DirectX::XMFLOAT2 position;
 		DirectX::XMFLOAT2 moveVec;
@@ -60,5 +62,9 @@ namespace Argent::Input
 		bool isWheelRotate;
 		float rowWheelRotateValue;
 		WheelStates wheelState;
+		bool resetPositionToCenter = false;
+
+		int width = GetSystemMetrics(SM_CXSCREEN);
+		int height = GetSystemMetrics(SM_CYSCREEN);
 	};
 }
