fd1d59badbd9f40e85bd816ae4aa543b46fc3d5f
ShinG24 <ShinGgm24@gmail.com>
2023-04-20 18:52:34 +0900

    ステージとのレイキャストは行ってます Y座標を少し調整する必要がある

--- Source/Stage.cpp
+++ Source/Stage.cpp
@@ -23,8 +23,8 @@ void Stage::Initialize()
 		}
 	}
 
-	//GetOwner()->AddComponent(new Argent::Component::Collider::RayCastCollider(mResource));
-	GetOwner()->AddComponent(new Argent::Component::Collider::RayCastCollider(Argent::Component::Collider::RayCastCollider::MeshType::Cube));
+	GetOwner()->AddComponent(new Argent::Component::Collider::RayCastCollider(mResource));
+	//GetOwner()->AddComponent(new Argent::Component::Collider::RayCastCollider(Argent::Component::Collider::RayCastCollider::MeshType::Cube));
 
 	//GetOwner()->AddComponent(new Argent::Component::Collider::RayCastCollider(r->GetMesh()->meshResource));
 	//GetOwner()->AddComponent(new Argent::Component::Collider::RayCastCollider(Argent::Component::Collider::RayCastCollider::MeshType::Cube));
